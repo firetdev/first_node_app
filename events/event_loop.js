320828560e5abd8a83d1225953967ea649320598
firetdev <futuregamesstore@gmail.com>
2026-01-04 01:59:57 -0600

    Added example from docs

---
+++ events/event_loop.js
@@ -0,0 +1,19 @@
+// Event loop examples from documentation, here for reference
+
+const baz = () => console.log('baz');
+const foo = () => console.log('foo');
+const zoo = () => console.log('zoo');
+
+const start = () => {
+  console.log('start');
+  setImmediate(baz);
+  new Promise((resolve, reject) => {
+    resolve('bar');
+  }).then(resolve => {
+    console.log(resolve);
+    process.nextTick(zoo);
+  });
+  process.nextTick(foo);
+};
+
+start();
